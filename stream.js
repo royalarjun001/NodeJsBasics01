3e24f12d63b5a855dab9e2cc673dceaeb3c77453
Arjun Kumar Verma <royalarjun001@gmail.com>
2021-05-08 07:52:04 +0000

    Benifit of reading file from stream

--- stream.js
+++ stream.js
@@ -20,6 +20,17 @@ readStream.on('data', chunk => {
 // wite data to new file named 'streamExample2.txt'
 readStream.on('data', chunk => {
     writeSteam.write(chunk);
-})
+});
+
+// Addition Information:
+// To read large file we should not be using readFile method available in node
+// Why: because readFile method use Buffer to read file and suppose we have file 
+//      size more then 2 GB then we need Buffer to be at least 2GB to read the file
+//      and that is less efficient and hence we should use stream methods to read 
+//      the data from the files.
+//
+// Stream also uses a buffer but not a full buffer it uses a very very small buffer
+// We are reading the data in chucks and not attempting to read whole file in once 
+// hence it could read 2GB files And it could be memory efficient at the same time.
 
 // file end
